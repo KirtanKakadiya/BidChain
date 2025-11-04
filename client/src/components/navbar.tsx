e92ee5f4675dd7e39e1c8221f95cb1dc2d087a6d
Kirtan Kakadiya <35823164+KirtanKakadiya@users.noreply.github.com>
2025-11-04 12:50:20 -0700

    bug: replacing <a> tag with Link tag

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -2,43 +2,46 @@ import React, { JSX } from 'react';
 import { PrimaryButton } from './button';
 import PersonIcon from '@mui/icons-material/Person';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 
 interface NavBarItemProps {
-  readonly text: string;
-  readonly path?: string;
-  readonly onClick?: () => void;
-  readonly icon?: JSX.Element;
+    readonly text: string;
+    readonly path?: string;
+    readonly onClick?: () => void;
+    readonly icon?: JSX.Element;
 }
 
 const NAVBAR_ITEMS: readonly NavBarItemProps[] = [
-  { text: 'Marketplace', path: '#' },
-  { text: 'Connect a wallet', path: '#' },
+    { text: 'Marketplace', path: '#' },
+    { text: 'Connect a wallet', path: '#' },
 ];
 
 export function NavBar(): JSX.Element {
-  return (
-    <header className="navbar">
-      <div className="navbar-container">
-        <div className="navbar-left">
-          <div className="navbar-logo">
-            <img
-              src="/bidchain-logo.svg"
-              alt="BidChain logo"
-              className="navbar-logo"
-            />
-          </div>
-          <span className="navbar-brand">BidChain</span>
-        </div>
+    return (
+        <header className="navbar">
+            <div className="navbar-container">
+                <div className="navbar-left">
+                    <div className="navbar-logo">
+                        <Link to="/">
+                            <img
+                                src="/bidchain-logo.svg"
+                                alt="BidChain logo"
+                                className="navbar-logo"
+                            />
+                        </Link>
+                    </div>
+                    <span className="navbar-brand">BidChain</span>
+                </div>
 
-        <div className="navbar-right">
-          <nav className="navbar-links">
-            <a href="#">Marketplace</a>
-            <a href="#">Connect a wallet</a>
-          </nav>
+                <div className="navbar-right">
+                    <nav className="navbar-links">
+                        <Link to="/marketplace">Marketplace</Link>
+                        <Link to="#">Connect a wallet</Link>
+                    </nav>
 
-          <PrimaryButton text="Login" icon={PersonIcon} />
-        </div>
-      </div>
-    </header>
-  );
+                    <PrimaryButton text="Login" icon={PersonIcon} />
+                </div>
+            </div>
+        </header>
+    );
 }
