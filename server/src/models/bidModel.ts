3c7d640c6faf11209656633482090ad9d2b77f4f
Eadan <eadan.lay@ucalgary.ca>
2025-11-22 16:58:55 -0700

    chore: added Bid model funcs and type interfaces

---
+++ server/src/models/bidModel.ts
@@ -0,0 +1,34 @@
+import { DbClient } from '../db/dbClient';
+import type { Bid, CreateBidArgs } from '../types/bidType';
+
+export interface BidModel {
+    readonly getBidById: (id: number) => Promise<Bid | undefined>;
+    readonly createBid: (data: CreateBidArgs) => Promise<Bid | undefined>;
+}
+
+export function createBidModel(db: DbClient): BidModel {
+    async function getBidById(id: number) {
+        return (
+            (await db.bid.findUnique({
+                where: { id },
+                include: { auction: true, bidder: true },
+            })) ?? undefined
+        );
+    }
+
+    async function createBid(data: CreateBidArgs) {
+        return db.bid.create({
+            data: {
+                amount: data.amount,
+                bidderId: data.bidderId,
+                auctionId: data.auctionId,
+            },
+            include: { auction: true, bidder: true },
+        });
+    }
+
+    return Object.freeze({
+        getBidById,
+        createBid,
+    });
+}
