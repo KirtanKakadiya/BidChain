86169596498e34f6b1668d3527ab40cd12446bbf
Eadan <eadan.lay@ucalgary.ca>
2025-11-22 17:43:45 -0700

    chore: refactored createBid to placeBid

--- server/src/resolvers/bidResolver.ts
+++ server/src/resolvers/bidResolver.ts
@@ -1,12 +1,12 @@
 import { GraphQLError } from 'graphql';
 import { BidModel } from '../models/bidModel';
 import { HTTP_CODES } from '../httpCodes';
-import { Bid, CreateBidArgs } from '../types/bidType';
+import { Bid, PlaceBidArgs } from '../types/bidType';
 
-export function createBidResolver({ bidModel }: { bidModel: BidModel }) {
-    async function createBid(
+export function placeBidResolver({ bidModel }: { bidModel: BidModel }) {
+    async function placeBid(
         _parent: unknown,
-        { data }: { data: CreateBidArgs }
+        { data }: { data: PlaceBidArgs }
     ): Promise<Bid> {
         const { amount, bidderId, auctionId } = data;
 
@@ -17,7 +17,7 @@ export function createBidResolver({ bidModel }: { bidModel: BidModel }) {
         }
 
         try {
-            return await bidModel.createBid(data);
+            return await bidModel.placeBid(data);
         } catch (error: any) {
             console.error(error);
             throw new GraphQLError(`Failed to create bid. ${error}`, {
