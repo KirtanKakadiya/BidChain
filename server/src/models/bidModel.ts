27a231f69d9232aeb708fa2add8ecc1625c56627
Eadan <eadan.lay@ucalgary.ca>
2025-11-22 17:43:45 -0700

    chore: refactored createBid to placeBid

--- server/src/models/bidModel.ts
+++ server/src/models/bidModel.ts
@@ -6,7 +6,7 @@ export interface BidModel {
     readonly placeBid: (data: PlaceBidArgs) => Promise<Bid>;
 }
 
-export function createBidModel(db: DbClient): BidModel {
+export function placeBidModel(db: DbClient): BidModel {
     async function getBidById(id: number) {
         return (
             (await db.bid.findUnique({
