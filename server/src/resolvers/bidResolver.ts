9da3fda7d1a041ccdd4ed8750043d83b76e67cdf
Eadan <eadan.lay@ucalgary.ca>
2025-11-26 16:51:23 -0700

    chore: added logic to bid resolver to check user balance and auction price before placing bid.

--- server/src/resolvers/bidResolver.ts
+++ server/src/resolvers/bidResolver.ts
@@ -2,8 +2,19 @@ import { GraphQLError } from 'graphql';
 import { BidModel } from '../models/bidModel';
 import { HTTP_CODES } from '../httpCodes';
 import { Bid, PlaceBidArgs } from '../types/bidType';
+import { UserModel } from '../models/userModel';
+import { AuctionModel } from '../models/auctionModel';
+import { configDotenv } from 'dotenv';
 
-export function createBidResolver({ bidModel }: { bidModel: BidModel }) {
+export function createBidResolver({
+    bidModel,
+    userModel,
+    auctionModel,
+}: {
+    bidModel: BidModel;
+    userModel: UserModel;
+    auctionModel: AuctionModel;
+}) {
     async function placeBid(
         _parent: unknown,
         { data }: { data: PlaceBidArgs }
@@ -16,8 +27,47 @@ export function createBidResolver({ bidModel }: { bidModel: BidModel }) {
             });
         }
 
+        // Attempt to retrieve user by ID
+        const user = await userModel.getUserById(bidderId);
+        if (!user) {
+            throw new GraphQLError(
+                'User not found while attempting to place bid.',
+                { extensions: { code: HTTP_CODES.NOT_FOUND } }
+            );
+        }
+
+        // Check if user has enough balance to place bid
+        if (user.walletBalance < amount) {
+            throw new GraphQLError('Insufficient wallet balance.', {
+                extensions: { code: HTTP_CODES.BAD_REQUEST },
+            });
+        }
+
+        // Attempt to retrieve auction by ID
+        const auction = await auctionModel.getAuctionById(auctionId);
+        if (!auction) {
+            throw new GraphQLError(
+                'Auction not found while attempting to place bid.',
+                { extensions: { code: HTTP_CODES.NOT_FOUND } }
+            );
+        }
+
+        // Verify bid is higher than current price
+        if (amount <= auction.currentPrice) {
+            throw new GraphQLError('Bid must be higher than current price.', {
+                extensions: { code: HTTP_CODES.BAD_REQUEST },
+            });
+        }
+
         try {
-            return await bidModel.placeBid(data);
+            const bid = await bidModel.placeBid(data);
+
+            // update auction's price
+            await auctionModel.updateAuction(auctionId, {
+                currentPrice: amount,
+            });
+
+            return bid;
         } catch (error: any) {
             console.error(error);
             throw new GraphQLError(`Failed to create bid. ${error}`, {
