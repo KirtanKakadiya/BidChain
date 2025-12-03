8034ffe4fbbbe8238bd7bd9f4a5f1498ae028203
Kirtan Kakadiya <35823164+KirtanKakadiya@users.noreply.github.com>
2025-12-02 17:04:57 -0700

    feature: create a method to close auctions

--- server/src/models/auctionModel.ts
+++ server/src/models/auctionModel.ts
@@ -6,11 +6,16 @@ import type {
     UpdateAuctionInput,
 } from '../types/auctionType';
 
+import type { Server as SocketIOServer } from 'socket.io';
+import { Bid } from '../types/bidType';
+
 export interface AuctionModel {
     readonly getAuctionById: (id: number) => Promise<Auction | undefined>;
     readonly getAuctionByNftId: (id: number) => Promise<Auction | undefined>;
     readonly createAuction: (data: CreateAuctionArgs) => Promise<Auction>;
     readonly updateAuction: (id: number, data: any) => Promise<Auction>;
+    readonly getHighestBid: (auctionId: number) => Promise<Bid | null>;
+    readonly closeExpiredAuctions: (io: SocketIOServer) => Promise<void>;
 }
 
 export function createAuctionModel(db: DbClient): AuctionModel {
@@ -57,10 +62,83 @@ export function createAuctionModel(db: DbClient): AuctionModel {
         });
     }
 
+    async function getHighestBid(auctionId: number) {
+        const highest = await db.bid.findFirst({
+            where: { auctionId },
+            orderBy: { amount: 'desc' },
+        });
+
+        return highest ?? null;
+    }
+
+    async function closeExpiredAuctions(io: SocketIOServer) {
+        const now = new Date();
+
+        try {
+            const expiredAuctions = await db.auction.findMany({
+                where: {
+                    isActive: true,
+                    endTime: {
+                        lte: now,
+                    },
+                },
+                include: {
+                    bids: true,
+                    nft: true,
+                },
+            });
+
+            if (expiredAuctions.length === 0) return;
+
+            for (const auction of expiredAuctions) {
+                console.log(auction);
+                const highestBid = await getHighestBid(Number(auction.id));
+
+                const winnerId = highestBid?.bidderId;
+                const sellerId = auction.nft.creatorId;
+
+                console.log(highestBid);
+                console.log(winnerId);
+                console.log(sellerId);
+
+                await db.auction.update({
+                    where: { id: auction.id },
+                    data: {
+                        isActive: false,
+                    },
+                });
+
+                if (!winnerId || !sellerId) return;
+
+                await db.user.update({
+                    where: { id: winnerId },
+                    data: {
+                        walletBalance: { decrement: highestBid?.amount },
+                    },
+                });
+
+                await db.user.update({
+                    where: { id: sellerId },
+                    data: {
+                        walletBalance: { increment: highestBid?.amount },
+                    },
+                });
+
+                console.log(
+                    `Auction ${auction.id} closed. Winner: ${winnerId}. Final price: ${highestBid?.amount}`
+                );
+            }
+        } catch (err) {
+            console.error('Error in closeExpiredAuctions:', err);
+        }
+    }
+
     return Object.freeze({
         getAuctionById,
         getAuctionByNftId,
         createAuction,
         updateAuction,
+        getHighestBid,
+        closeExpiredAuctions,
     });
 }
