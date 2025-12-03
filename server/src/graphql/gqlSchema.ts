0f09f3b1c1eccd75355ef991c3d7f76e35bd3513
Kirtan Kakadiya <35823164+KirtanKakadiya@users.noreply.github.com>
2025-12-02 17:03:45 -0700

    bug: highest bid amount returns a float

--- server/src/graphql/gqlSchema.ts
+++ server/src/graphql/gqlSchema.ts
@@ -154,6 +154,7 @@ export const gqlSchema = `#graphql
     #Auctions
     createAuction(data: CreateAuctionInput!): Auction!
     updateAuction(data: UpdateAuctionInput!): Auction!
+    highestBid(auctionId: Int!): Float!
 
     #Bids
     placeBid(data: placeBidArgs!): Bid!
