37617d9ebfc7a4441a7ecfab28eaa47a6d6806f9
Kirtan Kakadiya <35823164+KirtanKakadiya@users.noreply.github.com>
2025-12-04 10:30:31 -0700

    feature: add queries for getting users nfts

--- client/src/graphql/queries/userQueries.ts
+++ client/src/graphql/queries/userQueries.ts
@@ -11,3 +11,60 @@ export const FETCH_USER = `
     }
     `
 ;
+
+export const OWNED_NFTS_QUERY = `
+  query OwnedNfts($userId: Int!) {
+    ownedNfts(userId: $userId) {
+      id
+      title
+      imageUrl
+      creator {
+        id
+        name
+        avatarPicture
+      }
+      auction {
+        currentPrice
+      }
+    }
+  }
+`;
+
+export const BID_NFTS_QUERY = `
+  query BidNfts($userId: Int!) {
+    bidNfts(userId: $userId) {
+      id
+      title
+      imageUrl
+      creator {
+        id
+        name
+        avatarPicture
+      }
+      auction {
+        currentPrice
+      }
+    }
+  } 
+`;
+
+export const CREATED_NFTS_QUERY = `
+  query CreatedNFTs($creatorId: Int!) {
+    createdNfts(creatorId: $creatorId) {
+      id
+      title
+      description
+      imageUrl
+      createdAt
+      tags
+      creator {
+        id
+        name
+        avatarPicture
+      }
+      auction {
+        currentPrice
+      }
+    }
+  }
+`;
